8466bb8af235800e9836f0b03047e0a11ac35b88
jhuseynli <javid.huseynli@insa-rouen.fr>
2026-04-16 16:32:26 +0200

    Properly splits iterations into chunks now.

--- run_all.py
+++ run_all.py
@@ -2,18 +2,26 @@ import subprocess
 import os
 import sys
 
-TOTAL_ITERATIONS = 50
-CHUNK_SIZE = 5
+TOTAL_ITERATIONS = 100
+CHUNK_SIZE = 10
 CSV_FILE = "bint_batch_results.csv"
 
 if __name__ == "__main__":
     if os.path.exists(CSV_FILE):
         os.remove(CSV_FILE)
         print("Deleted old csv file.")
+    
+    total_chunks = (TOTAL_ITERATIONS + CHUNK_SIZE - 1) // CHUNK_SIZE
+    chunk_num = 1
 
     print(f"--- Starting sweep: {TOTAL_ITERATIONS} iterations ---")
 
     for start in range(0, TOTAL_ITERATIONS, CHUNK_SIZE):
-        subprocess.run([sys.executable, "run.py", str(CHUNK_SIZE), CSV_FILE], check=True)
+        current_chunk = min(CHUNK_SIZE, TOTAL_ITERATIONS-start)
+    
+        print(f"\n Chunk {chunk_num}/{total_chunks} | Iterations {start + 1} to {start + current_chunk}...")
+        subprocess.run([sys.executable, "run.py", str(current_chunk), CSV_FILE], check=True)
+        
+        chunk_num += 1
 
     print(f"Completed sweep! Saved to {CSV_FILE}")
